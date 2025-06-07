005e93ab88a7429f604056df6b869a9c8be203ec
pratham-9029 <saixerox5075@gmail.com>
2025-06-07 11:56:49 +0000

    first commit

---
+++ Loop/prog1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout<<j<<" ";
+        }
+        for(int k=5; k>=i; k--)
+        {
+            cout<<"    ";
+        }
+        for(int j=i; j>=1; j--)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
